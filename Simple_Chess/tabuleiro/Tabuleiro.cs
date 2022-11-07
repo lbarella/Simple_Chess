540e7777735e9651f9d3e5fa100d6ff094284852
Leonardo Barella <leonardo.barella@gmail.com>
2022-11-07 16:05:39 -0300

    Tratando erros de tabuleiro com exceções personalizadas.

--- Simple_Chess/tabuleiro/Tabuleiro.cs
+++ Simple_Chess/tabuleiro/Tabuleiro.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace tabuleiro
+﻿namespace tabuleiro
 {
     internal class Tabuleiro
     {
@@ -25,10 +19,47 @@ namespace tabuleiro
             return pecas[linha, coluna];
         }
 
+        // Acessa uma peça na matriz de peças.
+        public Peca peca(Posicao pos)
+        {
+            return pecas[pos.linha, pos.coluna];
+        }
+
+        // Verifica se há uma peça em dada posição válida.
+        public bool existePeca(Posicao pos)
+        {
+            validarPosicao(pos);
+            return peca(pos) != null;
+        }
+
+        // Coloca uma peça no tabuleiro.
         public void colocarPeca(Peca p, Posicao pos)
         {
+            if (existePeca(pos))
+            {
+                throw new TabuleiroException("Já existe uma peça nessa posição!");
+            }
             pecas[pos.linha, pos.coluna] = p;
             p.posicao = pos;
+        }
+
+        // Verifica se dada posição no tabuleiro é válida. 
+        public bool posicaoValida (Posicao pos)
+        {
+            if(pos.linha < 0 || pos.linha >= linhas  || pos.coluna < 0 || pos.coluna >= colunas)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        // Lança uma exceção se a posição não for válida.
+        public void validarPosicao(Posicao pos)
+        {
+            if (!posicaoValida(pos))
+            {
+                throw new TabuleiroException("Posição inválida!");
+            }
         }
     }
 }
