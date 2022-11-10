12568836ca2ea3ab36d7419f4b0541e2f76149d4
Leonardo Barella <leonardo.barella@gmail.com>
2022-11-10 16:35:46 -0300

    Melhorando a implementação do método imprimirTabuleiro.

--- Simple_Chess/Tela.cs
+++ Simple_Chess/Tela.cs
@@ -9,6 +9,7 @@ namespace Simple_Chess
         {
             for(int i = 0; i < tab.linhas; i++)
             {
+                Console.Write(8 - i + " ");
                 for(int j = 0; j < tab.colunas; j++)
                 {
                     if(tab.peca(i, j) == null) {
@@ -16,10 +17,29 @@ namespace Simple_Chess
                     }
                     else
                     {
-                        Console.Write(tab.peca(i, j) + " ");
+                        //Console.Write(tab.peca(i, j) + " ");
+                        imprimirPeca(tab.peca(i, j));
+                        Console.Write(" ");
                     }
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine("  A B C D E F G H");
+        }
+
+        public static void imprimirPeca(Peca peca)
+        {
+            if(peca.cor == Cor.Branca)
+            {
+                Console.Write(peca);
+            }
+            else
+            {
+                // Mudo o Foreground Color para amarelo e depois volto ele na cor padrão.
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
             }
         }
     }
